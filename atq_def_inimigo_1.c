e9f3197b34e4f452770b5632eb84ce2463fa6565
Matheus Brito <matheuswnb@kali.kaliwnb>
2025-11-30 18:32:33 -0300

    Inicializei os ataques do inimigo 1

---
+++ atq_def_inimigo_1.c
@@ -0,0 +1,14 @@
+#include "libs.h"
+#include "atq_def_inimigo_1.h"
+
+void func_atq_def_inimigo_1 (atq_def_inimigo_1 *ptr_atq_def_inimigo_1){
+    //Define os ataques do inimigo
+    ptr_atq_def_inimigo_1 -> ataques_basicos[0] = 5.0;
+    ptr_atq_def_inimigo_1 -> ataques_basicos[1] = 12.50;
+    ptr_atq_def_inimigo_1 -> ataques_basicos[3] = 18.50;
+
+    //Define as defesas do inimigo
+    ptr_atq_def_inimigo_1 -> defesas_basicas[0] = 50;
+    ptr_atq_def_inimigo_1 -> defesas_basicas[1] = 85;
+
+}
