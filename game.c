2a058eb04b6f7b81b1166dc8381e87cc11ec7275
Matheus Brito <matheuswnb@kali.kaliwnb>
2025-11-30 01:08:46 -0300

    Implementei as informações do jogador 1 na main. Tudo funcionando por enquanto :)

--- game.c
+++ game.c
@@ -1,10 +1,27 @@
 #include "libs.h"
 #include "ataques_e_defesas.h"
+#include "jogador_1.h"
 
 int main (){
+    //Inicializa ataques e defesas
     ataques_e_defesas escolha_ataques_e_defesas;
     func_ataques_e_defesas(&escolha_ataques_e_defesas);
 
+    //Inicializa jogador 1
+    info_jogador_1 inicializar_jogador_1;
+    func_info_jogador_1(&inicializar_jogador_1);
+
+    //Jogador 1
+    printf("Nome jogador 1 %s\n", 
+        inicializar_jogador_1.nome_jogador_1);
+
+    printf("Vida %d\n", 
+        inicializar_jogador_1.vida);
+
+    printf("Resistência %d\n", 
+        inicializar_jogador_1.resistencia);
+
+    //Ataques e defesas
     printf("Escolhas %d %d\n", 
         escolha_ataques_e_defesas.escolha[0], escolha_ataques_e_defesas.escolha[1]);
 
