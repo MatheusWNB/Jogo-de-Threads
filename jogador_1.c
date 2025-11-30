086947b2d7bcd782f755195a6730e5ca38da2eee
Matheus Brito <matheuswnb@kali.kaliwnb>
2025-11-30 01:42:48 -0300

    Criei a função de escolha de ataque do jogador 1. A escolha de ataque retorna pra main

--- jogador_1.c
+++ jogador_1.c
@@ -11,3 +11,17 @@ void func_info_jogador_1(info_jogador_1 *ptr_info_jogador_1){
 
 }
 
+int ataque_jogador_1(ataques_e_defesas *ptr_ataques_e_defesas){
+    int escolha_ataque;
+
+    printf(
+        "Escolha seu ataque: \n"
+        "Ataque básico 1 (dano: %0.2f). Pode ser usado toda rodada\n"
+        "Ataque básico 2 (dano: %0.2f). Pode ser usado a cada 2 rodadas\n", 
+        ptr_ataques_e_defesas->ataques_basicos[0], ptr_ataques_e_defesas->ataques_basicos[1]);
+
+    scanf("%d", &escolha_ataque);
+
+    return escolha_ataque;
+
+}
