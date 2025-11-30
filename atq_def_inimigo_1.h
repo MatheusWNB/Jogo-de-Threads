45d9546d1ddbcb63337ee5f42f58c22d4ffbc720
Matheus Brito <matheuswnb@kali.kaliwnb>
2025-11-30 18:33:51 -0300

    Declarando os ataques e defesas do inimigo 1

--- atq_def_inimigo_1.h
+++ atq_def_inimigo_1.h
@@ -1,11 +1,13 @@
 #ifndef ATQ_DEF_INIMIGO_1
 #define ATQ_DEF_INIMIGO_1
 
+//Declarando struct para ataques e defesas do inimigo 1
 typedef struct {
-    double ataques_basicos[2];
+    double ataques_basicos[3];
     double defesas_basicas[2];
 } atq_def_inimigo_1;
 
+//Função para definir os ataques e defesas
 void func_atq_def_inimigo_1 (atq_def_inimigo_1 *ptr_atq_def_inimigo_1);
 
 #endif
