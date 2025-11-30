ec33e7c7ec479e39d21951f235761ba4e590bd23
Matheus Brito <matheuswnb@kali.kaliwnb>
2025-11-30 01:06:10 -0300

    Adicionei o header do jogador 1. Struct das informações do jogador 1 e declarei função para iniciar as infos

---
+++ jogador_1.h
@@ -0,0 +1,21 @@
+#ifndef JOGADOR_1_H
+#define JOGADOR_1_H
+
+#include "ataques_e_defesas.h"
+
+typedef struct {
+    char nome_jogador_1[20];
+    int vida;
+    int resistencia;
+} info_jogador_1;
+
+//Função para inicializar as informações do jogador 1
+void func_info_jogador_1(info_jogador_1 *ptr_info_jogador_1);
+
+//Função para inicializar as ações do jogador 1 (Vou implementar)
+//void ataque_jogador_1(ataques_e_defesas *ptr_ataques_e_defesas);
+
+#endif
+
+
+
