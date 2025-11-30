f439257a42ca92b0597eb8d7f08549b9b6bad0ca
Matheus Brito <matheuswnb@kali.kaliwnb>
2025-11-30 05:00:43 -0300

    Declarei a função para ataque básico do jogador

--- jogador_1.h
+++ jogador_1.h
@@ -12,8 +12,11 @@ typedef struct {
 //Função para inicializar as informações do jogador 1
 void func_info_jogador_1(info_jogador_1 *ptr_info_jogador_1);
 
-//Função para inicializar as ações do jogador 1 (Vou implementar)
-int ataque_jogador_1(ataques_e_defesas *ptr_ataques_e_defesas);
+//Função para inicializar as ações do jogador 1 
+int escolha_ataque_jogador_1(ataques_e_defesas *ptr_ataques_e_defesas);
+
+//Função para realizar o ataque do jogador 1 
+double ataque_jogador_1(ataques_e_defesas *ptr_ataques_e_defesas, double vida_inimigo, int escolha_de_ataque);
 
 #endif
 
