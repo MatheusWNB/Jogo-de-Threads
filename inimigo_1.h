b49d79424eea4c8ce5d53f46a7f6ebe28f209078
Matheus Brito <matheuswnb@kali.kaliwnb>
2025-11-30 18:12:39 -0300

    Declarei a info do inimigo e a função para inicializar a info

---
+++ inimigo_1.h
@@ -0,0 +1,15 @@
+#ifndef INIMIGO_1_H
+#define INIMIGO_1_H
+
+typedef struct {
+    char nome[20];
+    double vida;
+    double resistência;
+} info_inimigo_1;
+
+void func_info_inimigo_1(info_inimigo_1 *ptr_info_inimigo_1);
+
+//int escolha_ataque_inimigo_1()
+
+#endif
+
