744c23755fa74b51a636f3073d86676e0aa7e3f3
Matheus Brito <matheuswnb@kali.kaliwnb>
2025-11-30 05:21:24 -0300

    Implementei a função de ataque do jogador 1, e ele da dano no inimigo. Coloquei um laço na função de escolha de ataque, pra evitar q uma opção inválida seja selecionada

--- jogador_1.c
+++ jogador_1.c
@@ -11,17 +11,40 @@ void func_info_jogador_1(info_jogador_1 *ptr_info_jogador_1){
 
 }
 
-int ataque_jogador_1(ataques_e_defesas *ptr_ataques_e_defesas){
+int escolha_ataque_jogador_1(ataques_e_defesas *ptr_ataques_e_defesas){
+    int laco = true;
     int escolha_ataque;
 
-    printf(
-        "Escolha seu ataque: \n"
-        "Ataque básico 1 (dano: %0.2f). Pode ser usado toda rodada\n"
-        "Ataque básico 2 (dano: %0.2f). Pode ser usado a cada 2 rodadas\n", 
-        ptr_ataques_e_defesas->ataques_basicos[0], ptr_ataques_e_defesas->ataques_basicos[1]);
+    while(laco){
 
-    scanf("%d", &escolha_ataque);
+        printf(
+            "Escolha seu ataque: \n"
+            "Ataque básico 0 (dano: %0.2f). Pode ser usado toda rodada\n"
+            "Ataque básico 1 (dano: %0.2f). Pode ser usado a cada 2 rodadas\n", 
+            ptr_ataques_e_defesas->ataques_basicos[0], ptr_ataques_e_defesas->ataques_basicos[1]);
+
+        scanf("%d", &escolha_ataque);
+
+        if (escolha_ataque < 0 || escolha_ataque > 1){
+            system("clear");
+            printf("Por favor, digite uma opção válida.\n");
+            continue;
+
+        } else if (escolha_ataque == 0 || escolha_ataque == 1) {
+            laco = false;
+
+        } else {
+            printf("Não era pra entrar aqui :)\n");
+        }
+    }
 
     return escolha_ataque;
+
+}
+
+double ataque_jogador_1(ataques_e_defesas *ptr_ataques_e_defesas, double vida_inimigo, int escolha_de_ataque){
+
+    vida_inimigo = vida_inimigo - ptr_ataques_e_defesas -> ataques_basicos[escolha_de_ataque];
+    return vida_inimigo;
 
 }
