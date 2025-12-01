2c96f5f70b788d582fe0249a06ed0c2296e10f8f
Matheus Brito <matheuswnb@kali.kaliwnb>
2025-12-01 20:32:50 -0300

    Declarei a função de escolha de ataque do inimigo 1

--- include/infos/inimigo_1.h
+++ include/infos/inimigo_1.h
@@ -9,7 +9,7 @@ typedef struct {
 
 void func_info_inimigo_1(info_inimigo_1 *ptr_info_inimigo_1);
 
-int escolha_ataque_inimigo_1();
+double escolha_ataque_inimigo_1(atq_def_inimigo_1 *ptr_atq_def_inimigo_1);
 
 #endif
 
