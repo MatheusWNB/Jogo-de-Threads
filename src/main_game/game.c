fda77868ef07f6587d31ccc46cc7e178cee2b5a9
Matheus Brito <matheuswnb@kali.kaliwnb>
2025-12-02 16:17:52 -0300

    Comecei a criar a interação de ataque com o usuário e o inimigo em rodadas. Irei arrumar o while pois não está encerrando quando a vida de alguém zera

--- src/main_game/game.c
+++ src/main_game/game.c
@@ -3,8 +3,12 @@
 #include "infos/jogador_1.h"
 #include "actions/atq_def_inimigo_1.h"
 #include "infos/inimigo_1.h"
+#include <time.h>
 
 int main (){
+    //Gera seed para escolha aleatória
+    srand ((unsigned) time(NULL));
+
     //Ataque escolhido
     int atq_jogador1;
     int atq_inmg1;
@@ -27,20 +31,43 @@ int main (){
     ataques_e_defesas escolha_ataques_e_defesas;
     func_ataques_e_defesas(&escolha_ataques_e_defesas);
 
-    //Usuário escolhe qual ataque usar
-    atq_jogador1 = escolha_ataque_jogador_1(&escolha_ataques_e_defesas);
+    int rodada_atual = 1;
+    int laco = true;
 
-    double vida_anterior_inmg1 = info_inmg1.vida;
-    info_inmg1.vida -= escolha_ataques_e_defesas.ataques_basicos[atq_jogador1];
+    while (true){
 
-    printf(
-        "Atacante: %s\n"
-        "Ataque escolhido: Ataque básico %d\n"
-        "Dano ao inimigo: Inimigo %s recebeu %0.2f de dano\n"
-        "Vida inimigo: %0.2f -> %0.2f\n",
-        info_jogador_1.nome_jogador_1, atq_jogador1, info_inmg1.nome,
-        escolha_ataques_e_defesas.ataques_basicos[atq_jogador1], vida_anterior_inmg1, info_inmg1.vida
-    );
+        //Usuário escolhe qual ataque usar
+        printf("RODADA %d. SUA VEZ DE JOGAR!\n", rodada_atual);
+        atq_jogador1 = escolha_ataque_jogador_1(&escolha_ataques_e_defesas);
+
+        double vida_anterior_inmg1 = info_inmg1.vida;
+        info_inmg1.vida -= escolha_ataques_e_defesas.ataques_basicos[atq_jogador1];
+
+        printf(
+            "Atacante: %s\n"
+            "Ataque escolhido: Ataque básico %d\n"
+            "Dano ao inimigo: Inimigo %s recebeu %0.2f de dano\n"
+            "Vida inimigo: %0.2f -> %0.2f\n",
+            info_jogador_1.nome_jogador_1, atq_jogador1, info_inmg1.nome,
+            escolha_ataques_e_defesas.ataques_basicos[atq_jogador1], vida_anterior_inmg1, info_inmg1.vida
+        );
+        printf("--------------------------------------------\n");
+
+        //Inimigo gera ataque aleatório
+        printf("RODADA %d. INIMIGO ESTÁ ATACANDO!\n", rodada_atual);
+        atq_inmg1 = escolha_ataque_inimigo_1(&escolha_atq_def_inmg1);
+        sleep(1.0);
+
+        if (info_inmg1.vida <= 0 || info_jogador_1.vida <= 0){
+            laco = false;
+        } else {
+            rodada_atual += 1;
+            continue;
+        }
+        
+    }
+
+
 
     return 0;
 }
