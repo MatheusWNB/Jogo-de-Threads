2ce381923a8f9167c729a36cbf021bd6c0c80ab2
Matheus Brito <matheuswnb@kali.kaliwnb>
2025-12-04 20:54:13 -0300

    Includes referentes ao inimigo e ao jogador

---
+++ include/libs/libs_inimigo.h
@@ -0,0 +1,8 @@
+#ifndef LIBS_INIMIGO_H
+#define LIBS_INIMIGO
+
+#include "actions/atq_def_inimigo_1.h"
+#include "infos/inimigo_1.h"
+#include "infos/info_rodada_atual.h"
+
+#endif
---
+++ include/libs/libs_jogador_1.h
@@ -0,0 +1,8 @@
+#ifndef LIBS_INIMIGO_H
+#define LIBS_INIMIGO
+
+#include "actions/ataques_e_defesas.h"
+#include "infos/jogador_1.h"
+#include "infos/info_rodada_atual.h"
+
+#endif
