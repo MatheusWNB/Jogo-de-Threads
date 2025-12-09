5f08b2e3ee071a883696cce76b862399c5ff3f48
Matheus Brito <matheuswnb@kali.kaliwnb>
2025-12-09 20:34:17 -0300

    Estruturei de uma maneira melhor o gerenciamento das threads utilizando funções separadas de ação do jogador e inimigo. A sincronização ainda está quebrada

--- src/main_game/game.c
+++ src/main_game/game.c
@@ -7,8 +7,8 @@
 #include "infos/info_rodada_atual.h"
 #include <pthread.h>
 #include <stdint.h>
-
-
+#include "actions/acao_inmg1.h"
+#include "actions/acao_jgdr.h"
 
 pthread_mutex_t mutex_1;
 #define THREADS_PRINCIPAIS 2
@@ -17,12 +17,6 @@ void *gerenciar_threads (void *arg, info_jogador_1 *info_jgdr, info_inimigo_1 *i
                         ataques_e_defesas *escolha_jgdr, atq_def_inimigo_1 *escolha_inmg, int *liberar_ataque, int *fim_rodada){
 
     pthread_mutex_init(&mutex_1, 0);
-    
-    
-    int atq_inmg1;
-    int atq_jogador1;
-    
-    double vida_anterior_inmg1;
     
     int rodada_atual = 1;
     int tid = (int)(intptr_t)arg;
@@ -39,20 +33,14 @@ void *gerenciar_threads (void *arg, info_jogador_1 *info_jgdr, info_inimigo_1 *i
                 }
             }
             
-            system("clear");
+            //system("clear");
             
             //Usuário escolhe qual ataque usar
             printf("RODADA %d\n           SUA VEZ DE JOGAR!          \n\n", rodada_atual);
-            atq_jogador1 = escolha_ataque_jogador_1(escolha_jgdr);//Escolha de ataque do jogador
-            putchar('\n');
-            
-            vida_anterior_inmg1 = info_inmg->vida;
-            info_inmg->vida -= escolha_jgdr->ataques_basicos[atq_jogador1]; //Reduz a vida do inimigo
-            
-            //Mostra as informações da jogada do jogador 1
-            info_rodada_atual_jgdr1(info_jgdr, info_inmg, escolha_jgdr, 
-                atq_jogador1, vida_anterior_inmg1);
-                
+
+            //Realiza a jogada do usuário
+            acao_do_jgdr(info_jgdr, info_inmg, escolha_jgdr);
+
             *liberar_ataque = true;
             pthread_mutex_unlock(&mutex_1);
         }
@@ -69,22 +57,20 @@ void *gerenciar_threads (void *arg, info_jogador_1 *info_jgdr, info_inimigo_1 *i
             if (*liberar_ataque == false){
                 pthread_mutex_unlock(&mutex_1);
             }
-                
-            atq_inmg1 = escolha_ataque_inimigo_1(escolha_inmg); //Escolha de ataque aleatório do inimigo 1
-            info_jgdr->vida -= escolha_inmg->ataques_basicos[atq_inmg1]; //Reduz a vida do jogador 1
-                
-            //Mostra as informações da jogada do inimigo
-            sleep(1.0);
-            info_rodada_atual_inmg1(info_jgdr, info_inmg, escolha_inmg, atq_inmg1);
+
+            //Realiza a jogada do inimigo 1
+            acao_do_inmg1(info_jgdr, info_inmg, escolha_inmg); 
                 
             *fim_rodada = true;
             pthread_mutex_unlock(&mutex_1);
         }   
     }
+
+    pthread_exit(NULL);
 }
     
 int main (){
-    int ataque_liberado_inimigo = true;
+    int ataque_liberado_inimigo = false;
     int *ptr_ataque_liberado_inimigo = &ataque_liberado_inimigo;
         
     int rodada_terminada = false;
@@ -106,8 +92,7 @@ int main (){
             
     //Inicializa ataques e defesas
     ataques_e_defesas escolha_ataques_e_defesas;
-    func_ataques_e_defesas(&escolha_ataques_e_defesas);
-            
+    func_ataques_e_defesas(&escolha_ataques_e_defesas);         
             
     int status, i;
     pthread_t threads[THREADS_PRINCIPAIS];
