8ac98fd9174ec655c6a5936fe4f6ef5c48d28919
Matheus Brito <matheuswnb@kali.kaliwnb>
2025-12-09 20:29:37 -0300

    Declaração de função para as ações do jogador e do inimigo

---
+++ include/actions/acao_inmg1.h
@@ -0,0 +1,9 @@
+#ifndef ACAO_INMG1_H
+#define ACAO_INMG1_H
+
+#include "libs/libs_inimigo.h"
+#include "libs/libs_jogador_1.h"
+
+void acao_do_inmg1(info_jogador_1 *info_jgdr, info_inimigo_1 *info_inmg, atq_def_inimigo_1 *escolha_inmg);
+
+#endif
---
+++ include/actions/acao_jgdr.h
@@ -0,0 +1,9 @@
+#ifndef ACAO_JGDR_H
+#define ACAO_JGDR_H
+
+#include "libs/libs_inimigo.h"
+#include "libs/libs_jogador_1.h"
+
+void acao_do_jgdr(info_jogador_1 *info_jgdr, info_inimigo_1 *info_inmg, ataques_e_defesas *escolha_jgdr);
+
+#endif
