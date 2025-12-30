47fce05e1de5ae14524a7a2fd56ce55a6ad0355d
Matheus Brito <matheuswnb@kali.kaliwnb>
2025-12-30 01:41:51 -0300

    fix: Consertei a geração de indices aleatórios para escolha de ataque do inimigo

--- src/infos/inimigo_1.c
+++ src/infos/inimigo_1.c
@@ -7,7 +7,7 @@
 void func_info_inimigo_1(info_inimigo_1 *ptr_info_inimigo_1) {
     
     strcpy(ptr_info_inimigo_1 -> nome, "Nine fingers");
-    ptr_info_inimigo_1 -> vida = 200;
+    ptr_info_inimigo_1 -> vida = 50;
     ptr_info_inimigo_1 -> resistência = 100;
 
 }
@@ -17,18 +17,9 @@ int escolha_ataque_inimigo_1 (atq_def_inimigo_1 *ptr_atq_def_inimigo_1){
     int laco = true;
     int indice;
 
-    while (laco){
-
-        int tamanho = sizeof(ptr_atq_def_inimigo_1 -> ataques_basicos) / (sizeof(8));
-        indice = rand() % tamanho;
-
-        if (indice >= 3)
-            continue;
-        
-        else
-            laco = false;
-
-    }
+    int tamanho = sizeof(ptr_atq_def_inimigo_1->ataques_basicos) / sizeof(double);
+    indice = rand() % tamanho;
 
     return indice;
+
 }
